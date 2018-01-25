5e7947c756cffe0d2b194a712af2563703bb9bd2
Idanord <33676867+Idanord@users.noreply.github.com>
2018-01-24 18:11:13 -0800

    App Commit
    Commit of the InvoiceTotalApp

---
+++ app/src/main/java/com/example/will/invoicetotalapp/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.will.invoicetotalapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends Activity implements TextView.OnEditorActionListener {
+
+    //declare instance variables for the widgets
+
+    private EditText inputEditText;
+    private TextView percentTextView;
+    private TextView discountTextView;
+    private TextView totalTextView;
+
+    private String subtotalString;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //get references to the widgets for the R class
+        inputEditText = (EditText) findViewById(R.id.inputEditText);
+        percentTextView = (TextView) findViewById(R.id.percentTextView);
+        discountTextView = (TextView) findViewById(R.id.discountTextView);
+        totalTextView = (TextView) findViewById(R.id.totalTextView);
+
+        inputEditText.setOnEditorActionListener(this);
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        //add calculatedAndDisplay
+
+        return false;
+    }
+
+    private void calculateAndDisplay(){
+        //get subtotal
+        subtotalString = inputEditText.getText().toString();
+        float subtotal;
+        //convert subtotal to sting
+        if(subtotalString.equals("")){
+            subtotal = 0f;
+        } else {
+            subtotal = Float.parseFloat(subtotalString);
+        }
+
+        //get the discount percent
+        float discountPercent = 0f;
+
+        //set discount percent
+        if(subtotal >= 200){
+            discountPercent = .2f;
+        } else if (subtotal >=100){
+            discountPercent = .1f;
+        } else {
+            discountPercent = 0;
+        }
+
+        //calculate the discount
+        float discountAmount = subtotal * discountPercent;
+        float total = subtotal - discountAmount;
+
+        //display the data on the layout
+        NumberFormat percent = NumberFormat.getPercentInstance();
+        percentTextView.setText(percent.format(discountPercent));
+
+        //display the discount amount
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        discountTextView.setText(currency.format(discountAmount));
+
+        //display the total
+        totalTextView.setText(currency.format(total));
+    }
+}
